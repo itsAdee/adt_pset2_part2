541774a844a01bf375a0e3a98a5102b40b6884f1
itsAdee <adeelahmedqureshi02@gmail.com>
2023-11-24 11:15:54 +0500

    Added Graph Poet Test

--- test/poet/GraphPoetTest.java
+++ test/poet/GraphPoetTest.java
@@ -16,7 +16,6 @@ import org.junit.Test;
 public class GraphPoetTest {
 
     // Testing strategy
-    // TODO
 
     @Test(expected = AssertionError.class)
     public void testAssertionsEnabled() {
@@ -25,10 +24,45 @@ public class GraphPoetTest {
 
     @Test
     public void testGraphPoetCreation() throws IOException {
-        File corpusFile = new File("test/poet/sample-corpus.txt");
+        File corpusFile = new File("test/poet/seven-words.txt");
         GraphPoet poet = new GraphPoet(corpusFile);
         // Add assertions to check if the graph is correctly constructed from the corpus
         // Check for specific vertices and edges based on the sample corpus
+    }
+
+    @Test
+    public void testPoemGenerationWithBridgeWords() throws IOException {
+        File corpusFile = new File("test/poet/seven-words.txt");
+        GraphPoet poet = new GraphPoet(corpusFile);
+
+        // Test with an input string requiring bridge words
+        String input1 = "Seek to explore new and exciting synergies!";
+        String poem1 = poet.poem(input1);
+        assertEquals("Seek to explore strange new life and exciting synergies!", poem1);
+    }
+
+    @Test
+    public void testPoemGenerationWithoutBridgeWords() throws IOException {
+        File corpusFile = new File("test/poet/seven-words.txt");
+        GraphPoet poet = new GraphPoet(corpusFile);
+
+        // Test with an input string where no bridge words are needed
+        String input2 = "This is a test.";
+        String poem2 = poet.poem(input2);
+        assertEquals("This is a test.", poem2);
+    }
+
+    @Test
+    public void testPoemGenerationWithUnknownWords() throws IOException {
+        File corpusFile = new File("test/poet/seven-words.txt");
+        GraphPoet poet = new GraphPoet(corpusFile);
+
+        // Test with an input string containing words not present in the corpus
+        String input3 = "Hello unknown world!";
+        String poem3 = poet.poem(input3);
+        // In this case, the unknown words should be preserved, and no bridge words
+        // inserted
+        assertEquals("Hello unknown world!", poem3);
     }
 
 }
