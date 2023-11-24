100075a12a975706aca14f820b8c414586123957
itsAdee <adeelahmedqureshi02@gmail.com>
2023-11-24 11:02:46 +0500

    Add Corpus Text File

--- test/poet/GraphPoetTest.java
+++ test/poet/GraphPoetTest.java
@@ -5,21 +5,30 @@ package poet;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
 
 /**
  * Tests for GraphPoet.
  */
 public class GraphPoetTest {
-    
+
     // Testing strategy
-    //   TODO
-    
-    @Test(expected=AssertionError.class)
+    // TODO
+
+    @Test(expected = AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
-    // TODO tests
-    
+
+    @Test
+    public void testGraphPoetCreation() throws IOException {
+        File corpusFile = new File("test/poet/sample-corpus.txt");
+        GraphPoet poet = new GraphPoet(corpusFile);
+        // Add assertions to check if the graph is correctly constructed from the corpus
+        // Check for specific vertices and edges based on the sample corpus
+    }
+
 }
